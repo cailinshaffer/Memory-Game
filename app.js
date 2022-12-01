7ad4a6f8e05f919fedfd1410e9fa9d2a1c232908
cailinshaffer <cailinshaffer@gmail.com>
2022-12-01 16:59:41 -0700

    initial html

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ console.log('script be scripting')
  
 // DOM SELECTORS
 const tile = document.querySelector('.tile')
+const result = document.querySelector('.result')
+const reset = document.querySelector('.reset')
+const start = document.querySelector('.start')
+const moves = document.querySelector('.moves')
+const instructions = document.querySelector('.instructions')
+const win = document.querySelector('.win')
+
+
+
