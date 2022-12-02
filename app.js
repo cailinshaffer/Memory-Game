80bfb9fc3daaad1cee65924b4b1e29fde876a427
cailinshaffer <cailinshaffer@gmail.com>
2022-12-02 16:22:59 -0700

    initial js

---
+++ app.js
@@ -0,0 +1,32 @@
+console.log('script be scripting')
+ 
+// // DOM SELECTORS
+// const result = document.querySelector('.result')
+// const reset = document.querySelector('.reset')
+// const start = document.querySelector('.start')
+// const moves = document.querySelector('.moves')
+// const instructions = document.querySelector('.instructions')
+// const win = document.querySelector('.win')
+// // const emojis = [🪂, ⛷, 🏂]
+
+let counter = 0;
+let firstSelector = "";
+let secondSelector = "";
+
+const tile = document.querySelectorAll('.tile');
+    
+// EVENT LISTENERS
+tile.forEach((tile) => {
+    tile.addEventListener('click', (e) => {
+        //console.log(true )
+        tile.classList.add('clicked')
+        if(counter === 0)
+        firstPlayer.card
+    }) 
+})
+
+
+ 
+
+
+
