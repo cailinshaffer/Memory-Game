64f49c8b534e5457de43eff203bfd3dad64c10cc
cailinshaffer <cailinshaffer@gmail.com>
2022-12-02 13:31:49 -0700

    initial java

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 console.log('script be scripting')
  
-// DOM SELECTORS
-const tile = document.querySelector('.tile')
-const result = document.querySelector('.result')
-const reset = document.querySelector('.reset')
-const start = document.querySelector('.start')
-const moves = document.querySelector('.moves')
-const instructions = document.querySelector('.instructions')
-const win = document.querySelector('.win')
+// // DOM SELECTORS
+// const result = document.querySelector('.result')
+// const reset = document.querySelector('.reset')
+// const start = document.querySelector('.start')
+// const moves = document.querySelector('.moves')
+// const instructions = document.querySelector('.instructions')
+// const win = document.querySelector('.win')
+// // const emojis = [🪂, ⛷, 🏂]
+
+let counter = 0;
+let firstPlayer = "";
+let secondPlayer = "";
+
+const tile = document.querySelectorAll('.tile');
+    
+// EVENT LISTENERS
+tile.forEach((tile) => {
+    tile.addEventListener('click', (e) => {
+        //console.log(true )
+        tile.classList.add('clicked')
+    })
+})
+
+
+ 
 
 
 
